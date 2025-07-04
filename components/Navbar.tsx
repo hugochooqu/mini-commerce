cc77ebdba6546bfada5deca69f09af2f3079413d
Ugochukwu Victor <hugotchee1@gmail.com>
2025-07-04 16:28:24 +0100

    fix import error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlineShopping, AiOutlineShoppingCart } from "react-icons/ai";
+import {  AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
   return (
